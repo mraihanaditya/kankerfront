3b5e2cadce83321da1205728c8b63a56f04e7912
mraihanaditya <m.raihanaditya.h@gmail.com>
2025-07-23 13:01:47 +0700

    new

--- app/detection/page.tsx
+++ app/detection/page.tsx
@@ -89,7 +89,7 @@ export default function DetectionPage() {
     setResult(null);
 
     try {
-      const response = await fetch("http://localhost:8000/predict", {
+      const response = await fetch("https://rehan-api.stegripe.org/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
